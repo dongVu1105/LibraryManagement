008968ab58a14354e76b68d1ccb854524fcdc511
Vu Huy Dong <huydongvu0105@gmail.com>
2025-05-27 21:11:58 +0700

    config security

---
+++ src/main/java/com/dongVu1105/libraryManagement/dto/response/IntrospectResponse.java
@@ -0,0 +1,14 @@
+package com.dongVu1105.libraryManagement.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class IntrospectResponse {
+    boolean valid;
+}
---
+++ src/main/java/com/dongVu1105/libraryManagement/exception/ErrorCode.java
@@ -0,0 +1,22 @@
+package com.dongVu1105.libraryManagement.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
+public enum ErrorCode {
+    UNAUTHENTICATED(1001, "authenticated error", HttpStatus.BAD_REQUEST)
+    ;
+
+
+    private long code;
+    private String message;
+    private HttpStatusCode httpStatusCode;
+
+    ErrorCode(long code, String message, HttpStatusCode httpStatusCode) {
+        this.code = code;
+        this.message = message;
+        this.httpStatusCode = httpStatusCode;
+    }
+}
