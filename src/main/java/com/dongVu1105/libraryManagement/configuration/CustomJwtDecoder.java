f017691c0170bdb692190e66c0b9a19e968910fe
Vu Huy Dong <huydongvu0105@gmail.com>
2025-05-27 21:28:18 +0700

    hide config

--- src/main/java/com/dongVu1105/libraryManagement/configuration/CustomJwtDecoder.java
+++ src/main/java/com/dongVu1105/libraryManagement/configuration/CustomJwtDecoder.java
@@ -28,14 +28,14 @@ public class CustomJwtDecoder implements JwtDecoder {
 
     @Override
     public Jwt decode(String token) throws JwtException {
-        try {
+//        try {
             var response = authenticationService.introspect(
                     IntrospectRequest.builder().token(token).build());
 
             if (!response.isValid()) throw new JwtException("Token invalid");
-        } catch (JOSEException | ParseException e) {
-            throw new JwtException(e.getMessage());
-        }
+//        } catch (JOSEException | ParseException e) {
+//            throw new JwtException(e.getMessage());
+//        }
 
         if (Objects.isNull(nimbusJwtDecoder)) {
             SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(), "HS512");
