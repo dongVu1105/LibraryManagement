bdff33806d7bb7b91f545c76286d64894d54bfe0
Vu Huy Dong <huydongvu0105@gmail.com>
2025-05-30 19:34:26 +0700

    add AccessDeniedException (for permisson when access function) and UncatorizedException

--- src/main/java/com/dongVu1105/libraryManagement/exception/ErrorCode.java
+++ src/main/java/com/dongVu1105/libraryManagement/exception/ErrorCode.java
@@ -9,7 +9,9 @@ public enum ErrorCode {
     UNAUTHENTICATED(1001, "authenticated error", HttpStatus.UNAUTHORIZED),
     USER_EXISTED(1002, "user existed", HttpStatus.BAD_REQUEST),
     USER_NOT_EXISTED(1003, "user not existed", HttpStatus.BAD_REQUEST),
-    ROLE_NOT_EXISTED(1004, "role not existed", HttpStatus.BAD_REQUEST)
+    ROLE_NOT_EXISTED(1004, "role not existed", HttpStatus.BAD_REQUEST),
+    UNAUTHORIZED(1005, "You do not have permission", HttpStatus.FORBIDDEN),
+    UNCATEGORIZED(1006, "uncategorized exception", HttpStatus.BAD_REQUEST)
     ;
 
 
